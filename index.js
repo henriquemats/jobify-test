26d71018163c798c943611905963eea7fd183dc5
henriquemats <henrique.projects28@gmail.com>
2020-01-16 12:44:00 -0300

    test

--- index.js
+++ index.js
@@ -8,16 +8,9 @@ const dbConnection = sqlite.open('banco.sqlite', { Promise })
 
 const port = process.env.PORT || 3000
 
-if (process.env.NODE_ENV === 'production') {
-    app.set('view engine', 'ejs')
-    app.use(express.static('public'));
-    app.get('/*', (request, response) => {
-        response.sendFile(path.join(__dirname, 'views'));
-    });
-}
-
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', async (request, response) => {
